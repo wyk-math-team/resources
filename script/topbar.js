de6aaa394db1f0c9e4296a7c05a6280c0940f1b9
wyk-math-team <dse.science.2028@gmail.com>
2026-09-17 21:32:43 +0800

    Update topbar.js

--- script/topbar.js
+++ script/topbar.js
@@ -483,7 +483,7 @@
   if (!document.querySelector('link[href*="font-awesome"]')) {
     const faLink = document.createElement('link');
     faLink.rel = 'stylesheet';
-    faLink.href = 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css';
+    faLink.href = 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css';
     document.head.appendChild(faLink);
   }
 })();
