f0e9193ac27f004c66b509b4e006d6f222be5ba4
wyk-math-team <dse.science.2028@gmail.com>
2026-09-20 11:41:01 +0800

    Update os-system.js

--- script/os-system.js
+++ script/os-system.js
@@ -1158,7 +1158,7 @@
           const now = Date.now();
 
           // ⭐ 500ms 內第二次點擊 → 視作雙擊，直接打開
-          if (now - lastClickTime < 500) {
+          if (now - lastClickTime < 300) {
             lastClickTime = 0;
             OS.launchIcon(id);
             return;
